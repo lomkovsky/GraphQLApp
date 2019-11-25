338bd70f205d97fbfec3d256fcbee1b3be00a9cc
lomkovsky <lomkovsky@gmail.com>
2019-11-25 17:50:56 +0200

    lesson 111

--- src/index.js
+++ src/index.js
@@ -1,23 +1,5 @@
-import { GraphQLServer, PubSub } from 'graphql-yoga';
-import db from './db';
-import { resolvers, fragmentReplacements } from './Resolvers/index';
-import prisma from './prisma';
-
-const pubsub = new PubSub();
-
-const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
-  resolvers,
-  context(request) {
-    return {
-      db,
-      pubsub,
-      prisma,
-      request,
-    }
-  },
-  fragmentReplacements,
-});
+import '@babel/polyfill/noConflict';
+import server from './server';
 
 server.start({ port: process.env.PORT }, () => {
   console.log('The server is up');
